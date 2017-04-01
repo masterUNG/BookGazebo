3a5fc0b5296fc0d9bfa613587fad6b72d5400676
masterUNG <phrombutr@gmail.com>
2017-04-01 17:06:45 +0700

    finish 1April

--- app/src/main/java/bangtanrut/songklod/bookgazebo/MainActivity.java
+++ app/src/main/java/bangtanrut/songklod/bookgazebo/MainActivity.java
@@ -1,13 +1,52 @@
 package bangtanrut.songklod.bookgazebo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View
+        initialView();
+
+        //Controller
+        controller();
+
+    }   // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //For TextView
+        if (view == textView) {
+            startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+        }
+
+    }   // onClick
+
+}   // Main Class
